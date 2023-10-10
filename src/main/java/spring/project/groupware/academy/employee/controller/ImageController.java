bf0fa016ae7e3ecf58874c135cf9fa8f1684a4dc
bin <bin7719@daum.net>
2023-10-10 17:48:15 +0900

    프로필 이미지 작업중

--- src/main/java/spring/project/groupware/academy/employee/controller/ImageController.java
+++ src/main/java/spring/project/groupware/academy/employee/controller/ImageController.java
@@ -1,44 +1,50 @@
-//package spring.project.groupware.academy.employee.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import spring.project.groupware.academy.employee.config.MyUserDetails;
-//import spring.project.groupware.academy.employee.dto.ImageUploadDto;
-//import spring.project.groupware.academy.employee.entity.EmployeeEntity;
-//import spring.project.groupware.academy.employee.service.ImageServiceImpl;
-//
-//@RequiredArgsConstructor
-//@Controller
-//@RequestMapping("/image")
-//public class ImageController {
-//
-//    private final ImageServiceImpl imageService;
-//
-//    // 이미지 등록
-//    @PostMapping("/upload")
-//    public String upload(@ModelAttribute ImageUploadDto imageUploadDto, @AuthenticationPrincipal MyUserDetails myUserDetails, Model model){
-//
-//        EmployeeEntity employee = myUserDetails.getEmployeeEntity();
-//        imageService.upload(imageUploadDto, employee.getEmployeeId());
-//
-//        // 이미지 URL을 모델에 추가
-//        model.addAttribute("memberImageUrl", "/profileImages/" + imageUploadDto.getFile().getOriginalFilename());
-//
-//        return "redirect:/member/detail/" + myUserDetails.getEmployeeEntity().getEmployeeNo();
-//    }
-//
-//    // 이미지 삭제
+package spring.project.groupware.academy.employee.controller;//package spring.project.groupware.academy.employee.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.project.groupware.academy.employee.config.MyUserDetails;
+import spring.project.groupware.academy.employee.dto.ImageUploadDto;
+import spring.project.groupware.academy.employee.entity.EmployeeEntity;
+import spring.project.groupware.academy.employee.service.EmployeeService;
+import spring.project.groupware.academy.employee.service.ImageService;
+import spring.project.groupware.academy.util.FileStorageService;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/image")
+public class ImageController {
+
+    private final FileStorageService fileStorageService;
+    private final ImageService imageService;
+    private final EmployeeService employeeService;
+
+    // 이미지 등록
+    @PostMapping("/upload")
+    public String upload(@ModelAttribute ImageUploadDto imageUploadDto, @AuthenticationPrincipal MyUserDetails myUserDetails, Model model) throws IOException {
+
+        EmployeeEntity employee = myUserDetails.getEmployeeEntity();
+        imageService.saveEmployeeImage(employee, imageUploadDto.getFile());
+
+        // 이미지 URL을 모델에 추가
+        model.addAttribute("employeeImageUrl", "/employeeImages/" + imageUploadDto.getFile().getOriginalFilename());
+
+        return "redirect:/employee/detail/" + myUserDetails.getEmployeeEntity().getEmployeeNo();
+    }
+
+    // 이미지 삭제
 //    @PostMapping("/delete")
 //    public String deleteImage(@AuthenticationPrincipal MyUserDetails myUserDetails) {
 //
 //        String employeeId = myUserDetails.getUsername();
-//        imageService.deleteImage(employeeId);
-//        return "redirect:/member/detail/" + myUserDetails.getEmployeeEntity().getEmployeeNo();
+//        fileStorageService.deleteImage(employeeId);
+//        return "redirect:/employee/detail/" + myUserDetails.getEmployeeEntity().getEmployeeNo();
 //    }
-//
-//}
+
+}
--- src/main/java/spring/project/groupware/academy/employee/service/ImageService.java
+++ src/main/java/spring/project/groupware/academy/employee/service/ImageService.java
@@ -1,13 +1,38 @@
-//package spring.project.groupware.academy.employee.service;
-//
-//
-//import spring.project.groupware.academy.employee.dto.ImageResponseDto;
-//import spring.project.groupware.academy.employee.dto.ImageUploadDto;
-//
-//public interface ImageService {
-//
-//    void upload(ImageUploadDto imageUploadDto, String employeeId);
-//    void deleteImage(String employeeId);
-//    ImageResponseDto findImage(String employeeId);
-//
-//}
+package spring.project.groupware.academy.employee.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import spring.project.groupware.academy.employee.entity.EmployeeEntity;
+import spring.project.groupware.academy.employee.entity.ImageEntity;
+import spring.project.groupware.academy.employee.repository.ImageRepository;
+import spring.project.groupware.academy.util.FileStorageService;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final FileStorageService fileStorageService;
+    private final ImageRepository imageRepository;
+
+    public void saveEmployeeImage(EmployeeEntity employeeEntity,
+                                  MultipartFile employeeImage) throws IOException {
+
+        System.out.println(employeeImage);
+
+        if (employeeImage != null && !employeeImage.isEmpty()) {
+            String savedPath = fileStorageService.storeFile("employee", employeeImage);
+
+            ImageEntity imageEntity = ImageEntity.builder()
+                    .employee(employeeEntity)
+                    .imageUrl(savedPath)
+                    .build();
+
+            imageRepository.save(imageEntity);
+        }
+    }
+
+}
