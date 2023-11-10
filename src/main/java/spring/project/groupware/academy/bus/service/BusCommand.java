cb5ad8560c399cb5853a05218ca947ed128e59fc
shengu9 <hgdh1013@gmail.com>
2023-11-10 14:39:02 +0900

    bus 정류장 도착시간 추가 1110_2

--- src/main/java/spring/project/groupware/academy/bus/service/BusCommand.java
+++ src/main/java/spring/project/groupware/academy/bus/service/BusCommand.java
@@ -4,20 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import spring.project.groupware.academy.chatbot.config.KeywordHandler;
 import spring.project.groupware.academy.chatbot.config.ServiceCommand;
+import spring.project.groupware.academy.chatbot.service.BusChatbotService;
 
 @Component
 @KeywordHandler("버스")
 public class BusCommand implements ServiceCommand {
 
-    private final BusChatService BusChatService;
+//    private final BusChatService BusChatService;
+    private final BusChatbotService BusChatBotService;
 
     @Autowired
-    public BusCommand(BusChatService BusChatService) {
-        this.BusChatService = BusChatService;
+    public BusCommand(BusChatbotService BusChatbotService) {
+        this.BusChatBotService = BusChatbotService;
     }
 
     @Override
     public String execute(String message) {
-        return BusChatService.BusChat(message);
+        return BusChatBotService.BusSearch(message);
     }
 }
